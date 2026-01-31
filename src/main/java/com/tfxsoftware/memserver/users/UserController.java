0477cbac488deb209046369d9b369506f82ce200
tfxsoftware <aswespeak.tfx@gmail.com>
2026-01-30 21:07:53 -0300

    test: add testing route for example on how to extract data from jwt and permissioning

--- src/main/java/com/tfxsoftware/memserver/users/UserController.java
+++ src/main/java/com/tfxsoftware/memserver/users/UserController.java
@@ -1,24 +1,32 @@
 package com.tfxsoftware.memserver.users;
 
 import lombok.RequiredArgsConstructor;
+
+import java.util.Map;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-
-import com.tfxsoftware.memserver.users.dto.CreateUserDto;
-
-import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
+@EnableMethodSecurity
 public class UserController {
 
-    private final UserService userService;
-
-    // TESTING ROUTE ONLY
-    @PostMapping
-    public ResponseEntity<User> registerUser(@Valid @RequestBody CreateUserDto dto) {
-        return ResponseEntity.ok(userService.createUser(dto));
+    //KEEPING THIS HERE FOR NOW -> TODO: DELETE LATER
+    @GetMapping("/me")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<?> getMyProfile(@AuthenticationPrincipal User currentUser) {
+        // If the JWT filter worked, 'currentUser' will be populated with your DB user
+        return ResponseEntity.ok(Map.of(
+            "message", "JWT is working!",
+            "email", currentUser.getEmail(),
+            "role", currentUser.getRole(),
+            "balance", currentUser.getBalance()
+        ));
     }
     
 }
