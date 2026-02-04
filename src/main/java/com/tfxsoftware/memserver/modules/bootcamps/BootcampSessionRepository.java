d2e33c8050f3a8e9db673130a085f663e12e4569
tfxsoftware <aswespeak.tfx@gmail.com>
2026-02-03 21:46:48 -0300

    feat: add query to find ready for tick

--- src/main/java/com/tfxsoftware/memserver/modules/bootcamps/BootcampSessionRepository.java
+++ src/main/java/com/tfxsoftware/memserver/modules/bootcamps/BootcampSessionRepository.java
@@ -1,10 +1,16 @@
 package com.tfxsoftware.memserver.modules.bootcamps;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 
 public interface BootcampSessionRepository extends JpaRepository<BootcampSession, UUID>{ 
     
+    @Query("SELECT s FROM BootcampSession s WHERE s.lastTickAt <= :threshold")
+    List<BootcampSession> findAllReadyForTick(@Param("threshold") LocalDateTime threshold);
 }
