870d05a0c1b16c0effffc60e6cb39eb9a1aeb18e
tfxsoftware <aswespeak.tfx@gmail.com>
2026-02-05 15:07:09 -0300

    refactor: remove commented-out code and unused imports in UserController

--- src/main/java/com/tfxsoftware/memserver/modules/users/UserController.java
+++ src/main/java/com/tfxsoftware/memserver/modules/users/UserController.java
@@ -5,10 +5,8 @@ import com.tfxsoftware.memserver.modules.players.dto.PlayerResponse;
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -21,18 +19,6 @@ public class UserController {
 
     private final PlayerService playerService;
 
-    //KEEPING THIS HERE FOR NOW -> TODO: DELETE LATER
-    @GetMapping("/me")
-    @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<?> getMyProfile(@AuthenticationPrincipal User currentUser) {
-        // If the JWT filter worked, 'currentUser' will be populated with your DB user
-        return ResponseEntity.ok(Map.of(
-            "message", "JWT is working!",
-            "email", currentUser.getEmail(),
-            "role", currentUser.getRole(),
-            "balance", currentUser.getBalance()
-        ));
-    }
 
     @GetMapping("/me/players")
     public ResponseEntity<List<PlayerResponse>> getMyPlayers(@AuthenticationPrincipal User user) {
