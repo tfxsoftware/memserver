1f36a23a8ce669e98685ae4f07a62e9ec5aa6a3c
tfxsoftware <aswespeak.tfx@gmail.com>
2026-02-17 16:07:50 -0300

    refactor: clean up unused imports and annotations in CreateRosterDto and UserController

--- src/main/java/com/tfxsoftware/memserver/modules/rosters/dto/CreateRosterDto.java
+++ src/main/java/com/tfxsoftware/memserver/modules/rosters/dto/CreateRosterDto.java
@@ -2,15 +2,11 @@ package com.tfxsoftware.memserver.modules.rosters.dto;
 
 import com.tfxsoftware.memserver.modules.users.User.Region;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.List;
-import java.util.UUID;
 
 @Data
 @NoArgsConstructor
--- src/main/java/com/tfxsoftware/memserver/modules/users/UserController.java
+++ src/main/java/com/tfxsoftware/memserver/modules/users/UserController.java
@@ -1,7 +1,6 @@
 package com.tfxsoftware.memserver.modules.users;
 
 import com.tfxsoftware.memserver.modules.players.PlayerService;
-import com.tfxsoftware.memserver.modules.players.dto.AddExperienceDto;
 import com.tfxsoftware.memserver.modules.players.dto.PlayerResponse;
 import com.tfxsoftware.memserver.modules.users.dto.UpdateUserDto;
 import com.tfxsoftware.memserver.modules.users.dto.UserResponse;
@@ -13,7 +12,6 @@ import java.util.UUID;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
